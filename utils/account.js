f78a04fa82ddbadacea0c4920629d7bfdb7d2cc0
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-06-07 09:50:53 +0200

    cleanup

--- utils/account.js
+++ utils/account.js
@@ -6,7 +6,6 @@ const salt = '$2a$10$W.aOVuY/hr48dEIgGsAjDO';
 
 function hashedPassword(password) {
     const hash = hashSync(password, salt);
-    // console.log(`Hashing: password="${password}", hash="${hash}"`)
     return hash;
 }
 
@@ -16,7 +15,6 @@ async function login(currentUser, status) {
     const [user] = await findUsers('username', currentUser.username);
     if (user) {
         const hash = hashSync(currentUser.password, salt);
-
         if (hash !== user.hashedPassword) {
             status.success = false;
             status.message = 'Wrong password.'
