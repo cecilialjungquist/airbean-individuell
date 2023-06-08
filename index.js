5345a48c0344e04f638fc61824bd1bff30b5b772
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-06-08 09:38:16 +0200

    Cleanup

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const express = require('express');
-
 const beansRoutes = require('./routes/beansRoutes.js');
 const userRoutes = require('./routes/userRoutes.js');
 const adminRoutes = require('./routes/adminRoutes.js');
 
+const express = require('express');
 const app = express();
 const PORT = 1337;
 
